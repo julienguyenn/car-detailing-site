7eb379b970ecd67a35319989ce6531821cb65936
Julie Nguyen <julie-nguyen@live.com>
2020-03-17 19:56:25 -0400

    connest psql with application

---
+++ server/seeds/01_seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO sample (id, name)
+VALUES (1, 'Julie');
--- server/server.js
+++ server/server.js
@@ -1,7 +1,26 @@
 
 const express = require('express')
+const { Pool } = require('pg')
 const app = express()
 const port = 8080
+
+
+const pool = new Pool({
+  user: 'julienguyen',
+  password: '123',
+  host: 'localhost',
+  database: 'cllective'
+});
+
+pool.query(`
+SELECT id, name, cohort_id
+FROM students
+LIMIT 5;
+`)
+.then(res => {
+  console.log(res);
+})
+.catch(err => console.error('query error', err.stack));
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
