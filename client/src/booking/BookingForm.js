4aaf72d2d3993da954f156357d1f3476099a715a
Julie Nguyen <julie-nguyen@live.com>
2020-03-20 18:07:27 -0400

    added date picker

--- client/src/booking/BookingForm.js
+++ client/src/booking/BookingForm.js
@@ -3,6 +3,14 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+
+
 // import Grid from '@material-ui/core/Grid';
 const axios = require('axios').default;
 
@@ -25,8 +33,7 @@ export default function BookingForm() {
   function changeData(event) {
     const key = event.target.name;
     if (key === "text") {
-      changeInputs({...inputs, "text": event.target.checked})
-      console.log(inputs.text)
+      changeInputs({...inputs, [key]: event.target.checked})
     } else {
       const value = event.target.value;
       changeInputs({...inputs, [key]: value})
@@ -56,6 +63,24 @@ export default function BookingForm() {
           control={<Checkbox checked={inputs.text} name="text" onChange={changeData} label="prefer texting"/>}
           label="prefer texting"
         />
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+
+          <KeyboardDatePicker
+            disableToolbar
+            variant="inline"
+            format="MM/dd/yyyy"
+            margin="normal"
+            id="date-picker-inline"
+            label="Date picker inline"
+            // value={selectedDate}
+            // onChange={handleDateChange}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+
+        </MuiPickersUtilsProvider>
+
       <Button type="submit" variant="contained">Submit</Button>
     </form>
   )
