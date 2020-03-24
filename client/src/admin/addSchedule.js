0b5590679ecc50ed773b495386847667d1e756f9
Julie Nguyen <julie-nguyen@live.com>
2020-03-23 20:04:42 -0400

    added route to add to schedule

--- client/src/admin/addSchedule.js
+++ client/src/admin/addSchedule.js
@@ -1,10 +1,17 @@
 import React from 'react';
+const axios = require('axios').default;
+
 
 export default function AddSchedule({}) {
+
+  function addTwoWeeks() {
+    axios.post('/addSchedule')
+  }
   return (
     <div>
-      <button>Add two weeks</button>
+      <button onClick={addTwoWeeks}>Add two weeks</button>
       <div>Change Availability</div>
+
     </div>
   )
 }
