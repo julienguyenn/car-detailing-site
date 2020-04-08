495b72e4c295bbdc0af412ec2732b65b78e2ef8b
Julie Nguyen <julie-nguyen@live.com>
2020-04-08 14:36:15 -0400

    formatted times to readable

--- client/src/booking/SingleTimeSlot.js
+++ client/src/booking/SingleTimeSlot.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export default function SingleTimeSlot({ start, end }) {
+  const formattedStart = Number(start) % 1 === 0.5 ? `${Number(start) - 0.5}:30` : `${Number(start)}:00`;
+  const formattedEnd = Number(end) % 1 === 0.5 ? `${Number(end) - 0.5}:30` : `${Number(end)}:00`;
   return (
     <button>
-      {start} to {end}
+      {formattedStart} to {formattedEnd}
     </button>
   )
 }
