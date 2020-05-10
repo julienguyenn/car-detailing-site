1b0fbe21fe4500c13c3ceec1b2a042765bfed43b
Julie Nguyen <julie-nguyen@live.com>
2020-05-10 13:50:18 -0400

    changed positioning of second half landing page

--- client/src/homePage/Homepage.js
+++ client/src/homePage/Homepage.js
@@ -6,11 +6,15 @@ export default function Homepage() {
   return (
     <div>
       <div className="large-box" id="banner">
-        <p className="slogan">Clean. Fast. Reliable.</p>
+        <p className="subtitle">Clean and Reliable.</p>
         <button className="generic-button">Learn more</button>
       </div>
       <div className="large-box2">
+        <div className="description-box">
+          <p className="subtitle">What can we do to help</p>
+        </div>
         <button className="visual-button services">SERVICES</button>
+        <div className="description-box"></div>
         <button className="visual-button contact">CONTACT INFO</button>
       </div>
     </div>
