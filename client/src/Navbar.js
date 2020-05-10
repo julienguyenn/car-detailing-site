50374dba94d7b85e031f0e9bcb2561bd28c640b3
Julie Nguyen <julie-nguyen@live.com>
2020-05-10 13:27:55 -0400

    changed background to black

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -8,9 +8,9 @@ export default function Navbar() {
       <div className="contents">
         <h1 class="company-name" id="navbar-name">CLLECTVE</h1>
         <div id="nav-buttons">
-          <a className="nav-but">Services</a>
-          <a className="nav-but">About</a>
-          <a className="nav-but">Contact</a>
+          <a className="nav-but">SERVICES</a>
+          <a className="nav-but">ABOUT</a>
+          <a className="nav-but">CONTACT</a>
         </div>
       </div>
     </nav>
