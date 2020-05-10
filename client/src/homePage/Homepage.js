fdb6a255c5c818ca0fb0901c1245eef7ec9ac7c1
Julie Nguyen <julie-nguyen@live.com>
2020-05-10 12:51:44 -0400

    positioned text in banner

--- client/src/homePage/Homepage.js
+++ client/src/homePage/Homepage.js
@@ -6,6 +6,9 @@ export default function Homepage() {
   return (
     <div>
       <div className="large-box" id="banner">
+        <p className="slogan">Clean. Fast. Reliable.
+          <br></br>
+          <span className="small-text">Learn more about us</span></p>
       </div>
       <div className="large-box2">
         <button className="visual-button services">SERVICES</button>
