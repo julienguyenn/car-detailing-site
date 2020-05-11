bb1e597cc974dcfd69349bb39e8848d21736f609
Julie Nguyen <julie-nguyen@live.com>
2020-05-11 15:57:40 -0400

    added padding in descriptions

--- client/src/homePage/Homepage.js
+++ client/src/homePage/Homepage.js
@@ -12,11 +12,14 @@ export default function Homepage() {
       <div className="large-box2">
         <div className="description-box">
           <p className="subtitle category">What can we do to help?</p>
-          <p className="description">Cllctve will try our best to make sure we can help maintain the quality of your car.</p>
+          <p className="description">At <span className="name">CLLCTIVE</span> we promise a high quality process and result to make sure your car looks its best, take a look at what we can offer for your vehicle.</p>
         </div>
-        <button className="visual-button services">SERVICES</button>
-        <div className="description-box"></div>
-        <button className="visual-button contact">CONTACT INFO</button>
+        <button className="visual-button services"><span className="generic-button">Our Services</span></button>
+        <div className="description-box">
+          <p className="description">Whether its questions about our practices or to book an appointment, don't be shy to contact us.</p>
+          <p className="subtitle category">Have questions? Get in touch.</p>
+        </div>
+        <button className="visual-button contact"><span className="generic-button">Contact Info</span></button>
       </div>
     </div>
   )
