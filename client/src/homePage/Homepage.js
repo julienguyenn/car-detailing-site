60d13d6bea8981f193cf9fe4936c420371f2a22b
Julie Nguyen <julie-nguyen@live.com>
2020-05-14 15:10:44 -0400

    redid dom for homepage

--- client/src/homePage/Homepage.js
+++ client/src/homePage/Homepage.js
@@ -13,13 +13,13 @@ export default function Homepage() {
         <div className="description-box">
           <p className="subtitle category">What can we do to help?</p>
           <p className="description">At <span className="name">CLLCTVE</span> we promise a high quality process and result to make sure your car looks its best, take a look at what we can offer for your vehicle.</p>
-        </div>
-        <div className="button-links">
-          <a className="visual-button">
-            <span className="button-text">Our Services</span>
-          </a>
-          <a className="visual-button"><span className="button-text">Contact Info</span>
-          </a>
+          <div className="button-links">
+            <a className="visual-button">
+              <span className="button-text">Our Services</span>
+            </a>
+            <a className="visual-button"><span className="button-text">Contact Info</span>
+            </a>
+          </div>
         </div>
       </div>
     </div>
