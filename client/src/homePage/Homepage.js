2f8d871f0cd07fb3a6f7030d28aebf7fe8e88a7f
Julie Nguyen <julie-nguyen@live.com>
2020-05-14 15:06:39 -0400

    removed background image in css

--- client/src/homePage/Homepage.js
+++ client/src/homePage/Homepage.js
@@ -14,9 +14,13 @@ export default function Homepage() {
           <p className="subtitle category">What can we do to help?</p>
           <p className="description">At <span className="name">CLLCTVE</span> we promise a high quality process and result to make sure your car looks its best, take a look at what we can offer for your vehicle.</p>
         </div>
-        <button className="visual-button services">
-          <span className="text">Our Services</span></button>
-        <button className="visual-button contact"><span className="generic-button">Contact Info</span></button>
+        <div className="button-links">
+          <a className="visual-button">
+            <span className="button-text">Our Services</span>
+          </a>
+          <a className="visual-button"><span className="button-text">Contact Info</span>
+          </a>
+        </div>
       </div>
     </div>
   )
