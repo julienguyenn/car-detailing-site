4424a6ac9c5618a33da4f85a419f1012d53b773a
Julie Nguyen <julie-nguyen@live.com>
2020-05-14 18:24:16 -0400

    added animate on scroll

--- client/src/homePage/Homepage.js
+++ client/src/homePage/Homepage.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import '../css/homepage.css'
+import ScrollAnimation from 'react-animate-on-scroll';
+
 
 export default function Homepage() {
 
   return (
     <div>
       <div className="large-box" id="banner">
+        
         <p className="subtitle">Clean and Reliable.</p>
         <button className="generic-button">Learn more</button>
       </div>
       <div className="large-box2">
+      <ScrollAnimation animateIn="fadeInUp">
         <div className="description-box">
-          <p className="subtitle category">What can we do to help?</p>
+            <p className="subtitle category">What can we do to help?</p>
           <p className="description">At <span className="name">CLLECTVE</span> we promise a high quality process and result to make sure your car looks its best, take a look at what we can offer for your vehicle.</p>
           <div className="button-links">
             <a className="visual-button">
@@ -25,6 +29,7 @@ export default function Homepage() {
             </a>
           </div>
         </div>
+      </ScrollAnimation>
       </div>
     </div>
   )
