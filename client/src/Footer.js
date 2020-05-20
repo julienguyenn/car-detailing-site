b24b05a48984548a631ccbf424e29adb767d906e
Julie Nguyen <julie-nguyen@live.com>
2020-05-20 17:57:25 -0400

    finished footer

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -5,10 +5,14 @@ import './css/Footer.css'
 export default function Footer() {
   return (
     <div id="footer">
-      {/* <a>
-        <h1 className="company-name">SOCIALS</h1>
-        <img alt="insta" src={require("./images/instagram.png")}></img>
-      </a> */}
+      <a>
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram">
+          <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+          <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+          <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+        </svg>
+      </a>
+      CLLECTVE
     </div>
   )
 }
