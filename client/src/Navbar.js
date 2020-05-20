37c8439bb82efe67e0f5f8f8035f252c9fba9dd2
Julie Nguyen <julie-nguyen@live.com>
2020-05-20 16:43:53 -0400

    added nav hamburger button

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -6,11 +6,13 @@ export default function Navbar() {
     <nav id="navbar">
       <div className="contents">
         <h1 class="company-name" id="navbar-name">CLLECTVE</h1>
-        <div id="nav-buttons">
-          <a className="nav-but">SERVICES</a>
-          <a className="nav-but">ABOUT</a>
-          <a className="nav-but">CONTACT</a>
-        </div>
+          <a className="nav-but">
+            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu">
+              <line x1="0" y1="12" x2="100" y2="12"></line>
+              <line x1="0" y1="6" x2="100" y2="6"></line>
+              <line x1="0" y1="18" x2="100" y2="18"></line>
+            </svg>
+          </a>
       </div>
     </nav>
   )
