4a41a139ecc0bd1d707f289401ec4e8f5a5d5040
Julie Nguyen <julie-nguyen@live.com>
2020-05-30 15:37:35 -0400

    added webpack again

---
+++ client/src/webpack.config.js
@@ -0,0 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: 'index.js',
+  output: {
+    path: '../dist',
+    filename: 'main.js'
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  devServer: {
+    historyApiFallback: true
+  }
+};
